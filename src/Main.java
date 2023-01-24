028f92ab14a13a01315f6caa9158b5822dc99f9d
Dominik <test@test.at>
2023-01-24 12:58:22 +0100

    Edit some lines...

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
         System.out.println("Bye world!");
-        System.out.println("Test!");
-        //erweitern
+        System.out.println("Some more output...");
     }
 }
